2fa78fc52c0feabab1eb35f0ffca485f4e8a3502
azoladyantyi <aizydyantyi@gmail.com>
2020-12-03 15:40:14 +0200

    I create a new class call Customer details,  connected my xamarin form to sqlite database

---
+++ T-ShirtOrderingApp/T-ShirtOrderingApp.Android/CustomerDetails.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace T_ShirtOrderingApp
+{
+    class CustomerDetails
+    {
+        public int Id 
+        { 
+            get; set; 
+        }
+        public string Name 
+        { 
+            get; set; 
+        }
+        public string Gender 
+        { 
+            get; set; 
+        }
+        public string TShirtSize 
+        { 
+            get; set; 
+        }
+        public DateTime DateofOrder 
+        {
+            get; set; 
+        }
+        public string ShippingAddress 
+        {
+            get; set; 
+        }
+
+
+    }
+}
--- T-ShirtOrderingApp/T-ShirtOrderingApp/MainPage.xaml.cs
+++ T-ShirtOrderingApp/T-ShirtOrderingApp/MainPage.xaml.cs
@@ -4,7 +4,9 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using T_ShirtOrderingApp;
 using Xamarin.Forms;
+using SQLite;
 
 namespace T_ShirtOrderingApp
 {
@@ -13,6 +15,23 @@ namespace T_ShirtOrderingApp
         public MainPage()
         {
             InitializeComponent();
+        }
+        void SaveBottun_Clicked(object sender, EventArgs e)
+        {
+            CustomerDetails CustomerDetails = new CustomerDetails()
+            {
+                Name = nameEntry.Text,
+                Gender = genderEntry.Text,
+                TShirtSize = TsizeEntry.Text,
+                DateofOrder = dateOdreEntry.Text,
+                TShirtColor = TSirtColorEntry.Text,
+                shippingAddress = shippingEntry.Text,
+            };
+            using (SQLiteConnection conn = new SQLiteConnection(App.FilePath))
+            {
+                conn.CreateTable<CustomerDetails>();
+                int rowsAdded = conn.Insert(CustomerDetails);
+            }
         }
     }
 }
